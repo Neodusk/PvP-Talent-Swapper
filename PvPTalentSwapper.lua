08f5cb57da497e336686c2afb99b597bdcc79798
Neodusk <jetou12@ymail.com>
2020-12-14 22:53:30 -0800

    Add initial files and function declarations

---
+++ PvPTalentSwapper.lua
@@ -0,0 +1,65 @@
+PvPTalentSwapper = { };
+--[[
+Retrieves the enemy classes and specialization
+@return {Array} enemies
+--]]
+function PvPTalentSwapper:getEnemyClasses()
+
+end
+
+--[[
+Retrieves the number of enemies on opposing team
+@return {Number} enemyCount
+--]]
+function PvPTalentSwapper:getEnemyCount()
+
+end
+
+--[[
+Retrieves the number of players on the players team
+Includes the player in the count
+@return {Number} teamCount
+--]]
+function PvPTalentSwapper:getTeamCount()
+
+end
+
+--[[
+Retrieves all custom talent sets for player
+--]]
+function PvPTalentSwapper:getTalentSets()
+
+end
+--[[
+Retrieves current custom talent set for player
+--]]
+function PvPTalentSwapper:getCurrentTalentSet()
+
+end
+
+--[[
+Sets custom talent set for player
+The player can define melee, caster, tank, heals for talent sets
+Example: 1 healer and 2 melee -> Set 1
+--]]
+function PvPTalentSwapper:setBasicTalentSet()
+
+end
+
+--[[
+Sets custom talent set for player
+The player can set talent sets for specific classes and specialization combinations
+Advanced talents defaultly override base talent sets
+--]]
+function PvPTalentSwapper:setAdvancedTalentSet()
+
+end
+
+--[[
+Re-orders the talent set override rule 
+--]]
+function PvPTalentSwapper:setTalentSetOrder()
+
+end
+
+
