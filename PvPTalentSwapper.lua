bdabf47874f25d881876863886da423f66819ec1
Neodusk <jetou12@ymail.com>
2020-12-15 13:07:40 -0800

    Add _ to function declarations to ignore self

--- PvPTalentSwapper.lua
+++ PvPTalentSwapper.lua
@@ -23,7 +23,7 @@ PvPTalentSwapper = { };
 Retrieves the enemy classes and specialization
 @return {Array} enemies
 --]]
-function PvPTalentSwapper:getEnemyClasses()
+function PvPTalentSwapper:getEnemyClasses(_)
 
 end
 
@@ -31,7 +31,7 @@ end
 Retrieves the number of enemies on opposing team
 @return {Number} enemyCount
 --]]
-function PvPTalentSwapper:getEnemyCount()
+function PvPTalentSwapper:getEnemyCount(_)
 
 end
 
@@ -40,7 +40,7 @@ Retrieves the number of players on the players team
 Includes the player in the count
 @return {Number} teamCount
 --]]
-function PvPTalentSwapper:getTeamCount()
+function PvPTalentSwapper:getTeamCount(_)
 
 end
 
@@ -49,25 +49,25 @@ Compares the number of players in each team
 If the enemy team count is less than player's, return false
 Else if the enemy team count is greater than player's, return true
 @return {Boolean}--]]
-function PvPTalentSwapper:compareTeamCounts()
+function PvPTalentSwapper:compareTeamCounts(_)
 
 end
 
 --[[
 Sets the talents to specified talents if other team has more
 members at the start of a game--]]
-function PvPTalentSwapper:setTalentOverride()
+function PvPTalentSwapper:setTalentOverride(_)
 
 end
 
 --[[
 Retrieves all custom talent sets for player--]]
-function PvPTalentSwapper:getTalentSets()
+function PvPTalentSwapper:getTalentSets(_)
 
 end
 --[[
 Retrieves current custom talent set for player--]]
-function PvPTalentSwapper:getCurrentTalentSet()
+function PvPTalentSwapper:getCurrentTalentSet(_)
 
 end
 
@@ -75,7 +75,7 @@ end
 Sets custom talent set for player
 The player can define melee, caster, tank, heals for talent sets
 Example: 1 healer and 2 melee -> Set 1--]]
-function PvPTalentSwapper:setBasicTalentSet()
+function PvPTalentSwapper:setBasicTalentSet(_)
 
 end
 
@@ -85,13 +85,13 @@ The player can set talent sets for specific classes and specialization combinati
 Allows the player to set custom sets which are chosen if there are more enemies on opposing team
 than on player's team
 Advanced talents defaultly override base talent sets--]]
-function PvPTalentSwapper:setAdvancedTalentSet()
+function PvPTalentSwapper:setAdvancedTalentSet(_)
 
 end
 
 --[[
 Re-orders the talent set override rule--]]
-function PvPTalentSwapper:setTalentSetOrder()
+function PvPTalentSwapper:setTalentSetOrder(_)
 
 end
 
