17ea0e88076011eb746f505816c7e4f076362c84
Neodusk <jetou12@ymail.com>
2020-12-15 07:08:42 -0800

    add additional testing code basis

--- test_spec.lua
+++ test_spec.lua
@@ -1,3 +1,51 @@
-describe("Test example", function ()
-  assert.is_true(true)
+
+--[[
+  Test constructor and deconstructor
+  Setup and Teardown for tests
+--]]
+describe("Test *constructor", function ()
+  
+  setup(function()
+  
+  end)
+
+  teardown(function()
+  
+  end)
+
+  before_each(function()
+  
+  
+  end)
 end)
+
+describe("getEnemyClasses", function()
+  pending("work in progress")
+end)
+
+describe("getEnemyCount", function()
+  pending("work in progress")
+end)
+
+
+describe("getEnemyCount", function()
+  pending("work in progress")
+end)
+
+describe("compareTeamCounts", function()
+  pending("work in progress")
+end)
+
+describe("setTalenOverride", function()
+  pending("work in progress")
+end)
+
+
+describe("getTalentSets", function()
+  pending("work in progress")
+end)
+
+describe("getCurrentTalentSet", function()
+  pending("work in progress")
+end)
+
